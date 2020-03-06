ffadeb89ea5e237b6e4b287f23d3fbf2d9a1691b
Michelle Morin <morin2m@gmail.com>
2020-03-06 08:56:04 -0800

    add enough logic to fail first test, fail first test

---
+++ WordCounter/Models/RepeatCounter.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace WordCounter.Models
+{
+  public class RepeatCounter
+  {
+    public static string ValidateWord(string inputWord)
+    {
+
+        return inputWord;
+    }
+
+  }
+}
